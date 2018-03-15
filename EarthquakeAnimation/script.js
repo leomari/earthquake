6c74a539199914856268d9db1d06b89be835d6ba
Endre Hauge Paulsen <35588575+endrehp@users.noreply.github.com>
2018-03-15 15:24:39 -0700

    Add files via upload

--- EarthquakeAnimation/script.js
+++ EarthquakeAnimation/script.js
@@ -14,16 +14,21 @@ map.on('load', function() {
       type: 'circle',
       source: {
         type: 'geojson',
-        data: './map_all.geojson' // replace this with the url of your own geojson
+        data: './map_all4.geojson' // replace this with the url of your own geojson
       },
       paint: {
-        'circle-radius': [
-          'interpolate',
+        'circle-radius':
+          
+          
+          [
+            'interpolate',
           ['linear'],
           ['number', ['get', 'S_Gal']],
         0, 4,
         5, 24
         ],
+        
+        
         'circle-color': [
           'interpolate',
           ['linear'],
@@ -40,6 +45,31 @@ map.on('load', function() {
             
           
         ],
+        
+        'circle-opacity': 0.8
+      }
+    }, 'admin-2-boundaries-dispute');
+    
+map.addLayer({
+      id: 'act',
+      type: 'circle',
+      source: {
+        type: 'geojson',
+        data: './map_all4.geojson' // replace this with the url of your own geojson
+      },
+      paint: {
+        'circle-radius': 6,
+          
+        'circle-color': ["step",
+            ['number', ['get', 'MdAct']] ,
+            '#000000',
+            0,'#000000',
+            1, '#2fff05'
+            
+        ]
+            
+          
+        ,
         'circle-opacity': 0.8
       }
     }, 'admin-2-boundaries-dispute');
@@ -62,22 +92,48 @@ for (var j=0; j < 100; j++) {
         
         document.getElementById('slider').value=i;
         var Time = i;
-        console.log(i);
+        
         i++;
         updateLayer(Time) }, j*100);
     }
 }
 });
     
+map.on('click', 'collisions', function (e) {
+        var coordinates = e.features[0].geometry.coordinates.slice();
+        var description = e.features[0].properties.Description;
+
+        // Ensure that if the map is zoomed out such that multiple
+        // copies of the feature are visible, the popup appears
+        // over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        new mapboxgl.Popup()
+            .setLngLat(coordinates)
+            .setHTML(description)
+            .addTo(map);
+    });
+
+    // Change the cursor to a pointer when the mouse is over the places layer.
+    map.on('mouseenter', 'places', function () {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+
+    // Change it back to a pointer when it leaves.
+    map.on('mouseleave', 'places', function () {
+        map.getCanvas().style.cursor = '';
+    });
+    
   
 });
 
 function updateLayer(Time) {
     map.setFilter('collisions', ['==', ['number', ['get', 'Time']], Time]);
-
+    map.setFilter('act', ['==', ['number', ['get', 'Time']], Time]);
+    
     document.getElementById('active-hour').innerText = Time;
-                            
+    
 }
-
-
 
